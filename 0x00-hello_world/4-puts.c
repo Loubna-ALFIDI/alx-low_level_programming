657f08c13301e2129a8393e6a4e287b5b542ec7d
Loubna <loubna.alfidi@gmail.com>
2023-03-16 14:34:20 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
-
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/**
-	 *  * main - Entry point
-	 *   *
-	 *    * Return: Always 0 (Success)
-	 *     */
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
