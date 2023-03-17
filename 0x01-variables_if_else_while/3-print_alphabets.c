c1b052bbdcef295871ef65bf98bd965dbafc4c3d
Loubna <loubna.alfidi@gmail.com>
2023-03-17 16:23:27 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,23 +6,12 @@
  */
 int main(void)
 {
-	int i = 0;
-	char alp[26] = {'a', 'b', 'c', 'd', 'e', 'e', 'f', 'g', 'h', 'i', 'j',
-	 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-	 'y', 'z'};
-	char ALP[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-	'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	char c;
 
-	while (i < 26)
-	{
-		putchar(alp[i]);
-		i++;
-	}
-	while (i < 26)
-	{
-		putchar(ALP[i]);
-		i++;
-	}
-	putch('\n');
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
 	return (0);
 }
