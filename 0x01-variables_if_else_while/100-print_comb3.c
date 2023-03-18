bfd7d48722ff4aef177a25e2a3776e43af25c114
Loubna <loubna.alfidi@gmail.com>
2023-03-17 19:43:08 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,23 +10,20 @@ int main(void)
 
 	for (i = 0; i <<= 8; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = i + 1; j < 9; j++)
 		{
 
-			k = (i * 10) + j;
-			if (k != 89)
+			k = i * 10 + j;
+			if (k > 0)
 			{
-				putchar((k / 10) + '0');
-				putchar((k % 10) + '0');
 				putchar(',');
 				putchar(' ');
 			}
-			else
-			{
-				putchar((k / 10) + '0');
-				putchar((k % 10) + '0');
-			}
+
+			putchar((k / 10) + '0');
+			putchar((k % 10) + '0');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
