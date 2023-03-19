e9fef7440693c46f21862a137866c443467dc421
Loubna <loubna.alfidi@gmail.com>
2023-03-19 08:58:21 -0700

    Software is eating the WorldSoftware is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l, m, n;
+
+	for (i = 0; i < 100; i++)
+	{
+		k = i / 10;
+		l = i % 10;
+		for (j = i + 1; j < 100; j++)
+		{
+
+			m = j / 10;
+			n = j % 10;
+			putchar(k + '0');
+			putchar(l + '0');
+			putchar(' ');
+			putchar(m + '0');
+			putchar(n + '0');
+			if ((k > 1) && (m > 1))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
