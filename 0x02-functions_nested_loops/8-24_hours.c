dcd4cb8380b70b05adada7a444bb05f3f0c9e06d
Loubna <loubna.alfidi@gmail.com>
2023-03-21 15:55:38 -0700

    Time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 		for (j = 0; j < 60; j++)
 		{
 			if (i < 10)
-				_putchar('0');
+				_putchar('0' + 0);
 			_putchar('0' + i);
 			_putchar(':');
 			if (j < 10)
-				_putchar('0');
+				_putchar('0' + 0);
 			_putchar('0' + j);
 			_putchar('\n');
 		}
