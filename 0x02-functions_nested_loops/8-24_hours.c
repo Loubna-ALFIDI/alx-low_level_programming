d937190f19fd86facfb41b67df413262841a85a2
Loubna <loubna.alfidi@gmail.com>
2023-03-21 15:51:20 -0700

    Time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+				_putchar('0');
+			_putchar('0' + i);
+			_putchar(':');
+			if (j < 10)
+				_putchar('0');
+			_putchar('0' + j);
+			_putchar('\n');
+		}
+	}
+}
