203f00e722e81c190b0816e2a928ab6a69b27288
Loubna <loubna.alfidi@gmail.com>
2023-03-21 19:04:31 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_to_98 - prints naturel numbers to 98
+ * @n: is a parameter
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 98; i++)
+	{
+		printf("%d, ", i);
+	}
+}
