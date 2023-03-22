55c39031e9f77627e786b45bdbf266e1e88bcf90
Loubna <loubna.alfidi@gmail.com>
2023-03-21 19:13:29 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,8 +10,10 @@ void print_to_98(int n)
 	int i;
 
 	for (i = n; i <= 98; i++)
-	{	if (i < 98)
-			printf("%d, ", i);
+	{
+		printf("%d", i);
+		if (i != 98)
+			printf(", ");
 	}
 	printf("\n");
 }
