c33575f2e2089285131dfd49143ac300f1920b50
Loubna <loubna.alfidi@gmail.com>
2023-03-21 19:30:41 -0700

    Sum of multiples pf 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	i = 1;
+	while (i % 3 == 0 || i % 5 == 0)
+	{
+		sum += i;
+		i++;
+	}
+	printf("%d\n", sum);
+}
