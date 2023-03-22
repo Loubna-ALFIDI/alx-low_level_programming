b77f17bc1b895f61081b5924e8c60fb0be1ddc75
Loubna <loubna.alfidi@gmail.com>
2023-03-22 12:12:37 -0700

    mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - prints if integer is positive or negative
+ * @i: is a parameter
+ * Return: void
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+}
