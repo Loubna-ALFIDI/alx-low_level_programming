56ad0d512906efa114e9c3abfe3628e7bed65a13
Loubna <loubna.alfidi@gmail.com>
2023-03-23 14:32:21 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: is a parameter
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
