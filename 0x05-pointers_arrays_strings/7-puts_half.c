99c57ccfd9e61769b0a7e05a08b87f922754fefc
Loubna <loubna.alfidi@gmail.com>
2023-03-27 19:41:58 -0700

    print half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,25 +6,25 @@
  */
 void puts_half(char *str)
 {
-    	 int j = 0;
-     	 int k;
-	
+	int j = 0;
+     	int k;
+
 	while (str[j] != '\0')	
 	{
 		j++;
 	}
-	 if (j % 2 == 1)
-	 {
-		 k = (j - 1) / 2;
-		 k += 1;
-	 }
-	 else
-	 {
-		 k = j / 2;
-	 }
-	 for (; k < j; k++)
-	 {
-		 _putchar(str[k]);
-	 }
-	 _putchar('\n');
+	if (j % 2 == 1)
+	{
+		k = (j - 1) / 2;
+		k += 1;
+	}
+	else
+	{
+		k = j / 2;
+	}
+	for (; k < j; k++)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
 }
