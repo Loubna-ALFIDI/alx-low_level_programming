ffcfb46c6902ab943c529452989833a3ea7fc621
Loubna <loubna.alfidi@gmail.com>
2023-03-27 18:41:58 -0700

    print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: is a parameter
+ * Return: void
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
