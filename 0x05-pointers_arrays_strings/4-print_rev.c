49f15247877af733e736a172678c79947e094081
Loubna <loubna.alfidi@gmail.com>
2023-03-27 18:08:30 -0700

    Reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,19 @@
 #include "main.h"
+/**
+ *print_rev - check the code
+ * @s: parametre
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int a;
 
-
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	for (a = a - 1; a >= 0; a--)
+		_putchar(s[a]);
+	_putchar('\n');
+}
