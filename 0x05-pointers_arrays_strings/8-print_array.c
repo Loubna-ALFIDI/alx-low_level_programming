5723ad1cee8378d598fd987b859a7ef816942325
Loubna <loubna.alfidi@gmail.com>
2023-03-27 20:10:03 -0700

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,8 +12,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
 		if (i != n - 1)
-			printf(", ");
+			printf("%d, ", a[i]);
+		else
+			printf`("%d", a[i]);
 	}
+	printf("\n");
 }
