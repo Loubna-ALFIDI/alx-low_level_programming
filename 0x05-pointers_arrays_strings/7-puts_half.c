001e0f1ae5970aca9947c2dc68a769950ac378f8
Loubna <loubna.alfidi@gmail.com>
2023-03-27 19:38:52 -0700

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - check the code
+ * @str: parametre
+ * Return: Always 0.
+ */
+void puts_half(charstr)
+{
+    	 int j = 0;
+     	 int k;
+	
+	while (str[j] != '\0')	
+	{
+		j++;
+	}
+	 if (j % 2 == 1)
+	 {
+		 k = (j - 1) / 2;
+		 k += 1;
+	 }
+	 else
+	 {
+		 k = j / 2;
+	 }
+	 for (; k < j; k++)
+	 {
+		 _putchar(str[k]);
+	 }
+	 _putchar('\n');
+}
