968bdb4efa74aeed16a0f78b4c9721df0f4230b7
Loubna <loubna.alfidi@gmail.com>
2023-03-27 19:22:06 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - prints array
+ * @a: is a parameter
+ * @n: is a parameter
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a + i);
+		if (i != n)
+			printf(", ");
+	}
+}
