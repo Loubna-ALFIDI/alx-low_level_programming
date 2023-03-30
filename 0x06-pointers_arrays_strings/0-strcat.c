13712032c54f631f62503a9fdee4e5bc3b879002
Loubna <loubna.alfidi@gmail.com>
2023-03-29 19:10:32 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatonates two strings
+ * @dest: is a parameter
+ * @src: is a parameter
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (*dest)
+	{
+		++dest;
+		i++;
+	}
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
