d7529652f91602bac7448f5436f359538b61bd07
Loubna <loubna.alfidi@gmail.com>
2023-03-29 19:59:30 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,20 +8,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, j = 0;
+	int j, i;
 
-	while (*dest)
-	{
-		++dest;
+	i = 0;
+	while (dest[i] != '\0')
 		i++;
-	}
-	while (*src)
+	j = 0;
+	while (src[j] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[j];
+		i++	
 		j++;
 	}
-	dest[j + i] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
