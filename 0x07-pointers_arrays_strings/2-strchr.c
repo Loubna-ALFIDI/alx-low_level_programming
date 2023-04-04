f704abc1517ffd8fe27a3c235c1be57a32b36fc9
Loubna <loubna.alfidi@gmail.com>
2023-04-03 20:01:43 -0700

    strhr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,9 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	while (s[i])
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-		{
-			return (s + i);
-		}
-		i++;
+			return (&s[i]);
 	}
-	return ('\0');
 }
