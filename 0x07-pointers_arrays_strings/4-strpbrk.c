e125e8c1b53185c002493ee362fdf802aa4bdee2
Loubna <loubna.alfidi@gmail.com>
2023-04-03 18:39:10 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strpbrk - gets the length of a prefix substring.
+ * @s: parameter
+ * @accept: parameter
+ * Return: char
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char c;
+
+	
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -19,4 +19,6 @@ void print_diagsums(int *a, int size);
 
 void set_string(char **s, char *to);
 
+int _putchar(char c);
+
 #endif /* MAIN_H */
