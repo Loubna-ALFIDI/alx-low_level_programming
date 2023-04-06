5cea260fd1819605542b144e155215e8e302d818
Loubna <loubna.alfidi@gmail.com>
2023-04-05 19:59:06 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,30 @@
 #include "main.h"
-
 /**
- * is_prime_number - checks if n is a prime number
- * @n: int
+ * check_if_divisible - check the code
+ * @n: parametre
+ * @i: parametre
+ * Return: int
+ */
+int check_if_divisible(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (check_if_divisible(n, i + 2));
+}
+/**
+ * is_prime_number - check the code
+ * @n: parametre
  * Return: int
  */
 int is_prime_number(int n)
 {
-	int i =    
-	if ()
+	if (n < 2)
+		return (0);
+	else if (n == 2)
+		return (1);
+	else if (n % 2 == 0)
+		return (0);
+	return (check_if_divisible(n, 3));
+}
