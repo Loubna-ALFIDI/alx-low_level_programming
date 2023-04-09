a1538a97d4f6aca7446fe20c68e6bd7897dc3386
Loubna <loubna.alfidi@gmail.com>
2023-04-08 17:12:55 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry poit
+ * @argc: int
+ * @argv: char*
+ * Return: 0
+ */
+int main(int argc __attribute__ ((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
