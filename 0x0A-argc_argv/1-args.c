9e7ecabad59277b6aae947b269b996307d2072d4
Loubna <loubna.alfidi@gmail.com>
2023-04-08 17:41:20 -0700

    number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: char*
+ * Return : 0
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
