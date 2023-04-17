2caf68e42cf8ff2800860298071ba69e0e0c14e3
Loubna <loubna.alfidi@gmail.com>
2023-04-17 15:20:40 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define
+/**
+ * struct dog - new type dog
+ * @name: char*
+ * @age: float
+ * @owner: char*
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
