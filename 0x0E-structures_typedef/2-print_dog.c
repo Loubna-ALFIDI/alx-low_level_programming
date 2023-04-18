93b679acc89646532e2cef57f0bcbbcc32839485
Loubna <loubna.alfidi@gmail.com>
2023-04-17 19:22:26 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,9 +12,9 @@ void print_dog(struct dog *d)
 	if (!d)
 		return;
 	if (!d->name)
-		p->name = "(nil)";
+		d->name = "(nil)";
 	if (!d->owner)
-		p->owner = "(nil)";
+		d->owner = "(nil)";
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
