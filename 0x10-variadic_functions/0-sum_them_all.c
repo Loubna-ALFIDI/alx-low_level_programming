9b5b4e96a9e837f43c8c1911ea39fa9feaf3bff3
Loubna <loubna.alfidi@gmail.com>
2023-04-20 16:50:16 -0700

    sum of args

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,18 +4,19 @@
 /**
  * sum_them_all - the sum of all its parameters.
  * @n: const unsigned int
+ * ...: args
  * Return: int
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, sm = 0;
+	unsigned int i, sum = 0;
 	va_list ap;
 
 	if (!n)
 		return (0);
-	va_list(ap, n);
-	for (i = n; i != 0; i = var_arg(ap, unsigned int))
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
 		sum += i;
-	var_end(ap);
+	va_end(ap);
 	return (sum);
 }
