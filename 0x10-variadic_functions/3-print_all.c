114d1b714329c5c7c4d9e34819138da1404e6aa0
Loubna <loubna.alfidi@gmail.com>
2023-04-20 19:47:45 -0700

    print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,38 +10,41 @@
  */
 void print_all(const char * const format, ...)
 {
-	const char *f = format;
+	int i = 0;
 	va_list ap;
 	char *s;
+	char *sep = "";
 
 	va_start(ap, format);
-	while (*f)
+	if (format)
 	{
-		switch (*f)
+		while (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(ap, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(ap, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(ap, double));
-				break;
-			case 's':
-				s = va_arg(ap, char*);
-				if (!s)
-					printf("(nil)");
-				else
-					printf("%s", s);
-				break;
-			default:
-				break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(ap, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(ap, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(ap, double));
+					break;
+				case 's':
+					s = va_arg(ap, char*);
+					if (s == NULL)
+						s = "(nil)";
+					printf("%s%s", sep, s);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
 		}
-		++f;
-		if (format && (*f == 'c' || *f == 'i' || *f == 'f' || *f == 's'))
-			printf(", ");
 	}
-	va_end(ap);
 	printf("\n");
+	va_end(ap);
 }
