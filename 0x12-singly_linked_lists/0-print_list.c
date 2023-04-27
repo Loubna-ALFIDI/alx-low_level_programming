f4067c8aa6244f536fddbdfc49bd39253c23b1fe
Loubna <loubna.alfidi@gmail.com>
2023-04-27 15:30:06 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list
+ * @h: const list_t
+ * Return: size_t, number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)");
+		printf("[%d] %s", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
