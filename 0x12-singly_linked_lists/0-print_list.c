25a9372504d9030845f6bdb426adbb811e4e2961
Loubna <loubna.alfidi@gmail.com>
2023-04-27 15:48:37 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,9 +12,9 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		n++;
 	}
