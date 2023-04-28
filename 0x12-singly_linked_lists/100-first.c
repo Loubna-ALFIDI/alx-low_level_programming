679541cb765cc907761f5e18ddc7fa7b9289e3e9
Loubna <loubna.alfidi@gmail.com>
2023-04-27 18:47:12 -0700

    print before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * printf_message - prints message before main
+ * print_message - prints message before main
  *
  * Return: void
  */
@@ -9,5 +9,6 @@ void print_message(void) __attribute__((constructor));
 
 void print_message(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
