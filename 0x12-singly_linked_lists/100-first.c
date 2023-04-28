26e596b9eabdc89caa90f776ffe67ea41d029b13
Loubna <loubna.alfidi@gmail.com>
2023-04-27 19:02:07 -0700

    print before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@ void print_message(void) __attribute__((constructor));
 
 void print_message(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    printf("(A tortoise, having pretty good sense of a hare's nature,
-	    challenges one to a race.)\n");
+    printf("(A tortoise, having pretty good sense of a hare's nature,");
+    printf("challenges one to a race.)\n");
     return (0);
 }
