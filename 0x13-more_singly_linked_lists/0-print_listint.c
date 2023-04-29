c6e9eea26fc3d0a42143bd98453d298c722e9dd7
Loubna <loubna.alfidi@gmail.com>
2023-04-29 15:35:08 -0700

    print a list of ints

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t n = 0;
+	size_t l = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		n++;
+		l++;
 	}
-	return (n);
+	return (l);
 }
