90346eb457a95f15d7d892468c0f2cfbd633f801
Loubna <loubna.alfidi@gmail.com>
2023-04-29 17:41:00 -0700

    list lenght

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list_t list.
+ * listint_len - returns the number of elements in a linked listint_t list.
  * @h: const list_l
  * Return: size_t
  */
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
-        size_t n = 0;
+        size_t l = 0;
 
         while (h)
         {
                 h = h->next;
-                n++;
+                l++;
         }
-        return (n);
+        return (l);
 }
