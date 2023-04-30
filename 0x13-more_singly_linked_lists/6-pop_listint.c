1498839e39e4297667c59f8bc4029738df5f5305
Loubna <loubna.alfidi@gmail.com>
2023-04-30 10:17:37 -0700

    print a list of ints

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list,
+ *  and returns the head node’s data (n).
+ * @head: listint_t **
+ * Return: int
+ */
+int pop_listint(listint_t **head)
+{
+
