17fc15fb417eadbdc7f907152a010da340ba034d
Loubna <loubna.alfidi@gmail.com>
2023-04-30 16:25:55 -0700

    get nth

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	if (!head)
 		return (0);
-	if(index == 0)
+	if (index == 0)
 		return (head->next);
-	return get_nodeint_at_index(head->next, index-1);
+	return (get_nodeint_at_index(head->next, index - 1));
 }
