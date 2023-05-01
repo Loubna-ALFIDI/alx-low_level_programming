cfd63fb3ae2cb1b6220b76a183558ec850efb8af
Loubna <loubna.alfidi@gmail.com>
2023-04-30 17:33:52 -0700

    Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (!head)
 		return (0);
 	if (index == 0)
-		return (head->next);
+		return (head);
 	return (get_nodeint_at_index(head->next, index - 1));
 }
