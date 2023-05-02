6132c2bcac568282fd13070fd5283c057209d4f0
Loubna <loubna.alfidi@gmail.com>
2023-05-01 19:00:21 -0700

    delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	current = *head;
-	while (i < index - 1 && current)
+	while (i < index  && current)
 	{
 		current = current->next;
 		i++;
