bc5cbb35d1ad5055bd042a3c750480301ae2b28b
Loubna <loubna.alfidi@gmail.com>
2023-05-01 19:04:11 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (node);
 	}
 	current = *head;
-	for (i = 0; i < idx && current; i++)
+	for (i = 0; i < idx - 1 && current; i++)
 		current = current->next;
 	if (!current)
 		return (NULL);
