676d7931385c02330f1d92fa28314f025aa68e6e
Loubna <loubna.alfidi@gmail.com>
2023-05-02 07:07:35 -0700

    insert at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,10 +12,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node, *current;
 	unsigned int i = 0;
 
-	if (!*head)
-		return (NULL);
 	node = malloc(sizeof(listint_t));
-	if (!node)
+	if (!node || !head)
 		return (NULL);
 	node->n = n;
 	if (idx == 0)
