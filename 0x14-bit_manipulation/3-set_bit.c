654a70dd49bcd158986cc86c30b51d7f9143bc01
Loubna <loubna.alfidi@gmail.com>
2023-05-04 16:19:58 -0700

    set bit to 1 at index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,7 +4,7 @@
  * set_bit -  sets the value of a bit to 1 at a given index.
  * @n: unsigned long int
  * @index: index starting from 0
- * Return 1 if success, -1 if it fails
+ * Return: 1 if success, -1 if it fails
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
