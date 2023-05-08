0aaa1e0e18143b3e4fa644d37add18e106fd8603
Loubna <loubna.alfidi@gmail.com>
2023-05-08 16:49:05 -0700

    read

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * @filename: const char *
+ * @letters: size_t
+ * Return: size_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *c;
+	ssize_t a, b, file;
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	c = malloc(sizeof(char) * letters);
+	a = read(file, c, letters);
+	b = write(STDOUT_FILENO, c, a);
+	free(c);
+	close(file);
+	return (b);
+}
