23840c076af6e43618fc2981ff84d10fd519b47e
Loubna <loubna.alfidi@gmail.com>
2023-05-08 16:50:20 -0700

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - read a text file and prints it to the POSIX standard output.
  * @filename: const char *
  * @letters: size_t
  * Return: size_t
