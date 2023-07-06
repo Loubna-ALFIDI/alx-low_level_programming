c6a57dd8b0609f1fec6d68ea69a4b68e2ebdda13
Loubna ALFIDI <loubna.alfidi@gmail.com>
2023-07-06 10:35:08 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        int i;
-        unsigned int number = 0;
+	int i;
+	unsigned int number = 0;
 
-        if (!b)
-                return (0);
+	if (!b)
+		return (0);
 
-        for (i = 0; b[i]; i++)
-        {
-                if (b[i] < '0' || b[i] > '1')
-                        return (0);
-                number = 2 * number + (b[i] - '0');
-        }
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		number = 2 * number + (b[i] - '0');
+	}
 
-        return (number);
+	return (number);
 }
