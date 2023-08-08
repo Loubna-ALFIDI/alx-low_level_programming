d751eeb9820f046f98d6f35b107139b2d62a7957
Loubna ALFIDI <loubna.alfidi@gmail.com>
2023-08-07 19:14:51 -0700

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: the name of the file.
+ * @text_content: the NULL terminated string to add at the end of the file.
+ * Return: 1 on success and -1 on failure.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+    	int file_des, t, a;
+
+	if (filename == NULL)
+		return (-1);
+	t = 0;
+	if (text_content != NULL)
+		while (text_content[t])
+			t++;
+	file_des = open(filename, O_WRONLY | O_APPEND);
+	a = write(file_des, text_content, t);
+	if (a == -1)
+		return (-1);
+	close(file_des);
+	return (1);
+}
