68d44237ef83ea683abdb9178e3d7c56e49e55c6
Loubna <loubna.alfidi@gmail.com>
2023-09-15 01:31:13 +0100

    Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a dlistint_t list.
+ * @head: dlistint_t
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
