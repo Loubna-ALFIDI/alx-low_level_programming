fe50df11ac801766da7872c3a7824a0c13ad1a62
Loubna <loubna.alfidi@gmail.com>
2023-09-19 01:26:48 +0100

    Without libraries what have we? We have no past and no future

---
+++ 0x18-dynamic_libraries/operation.c
@@ -0,0 +1,61 @@
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+ * div - check the code
+ * @a: parametre
+ * @b: parametre
+*/
+int div(int a, int b)
+{
+    return (a / b);
+}
+/**
+* add - adds two integers
+* @a: parametre
+* @b: parametre
+* Return: the sum
+*/
+
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+* sub - checks the code
+* @a: parametre
+* @b: parametre
+* Return: the subtract
+*/
+
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+* mul - multiplies two integers
+* @a: parametre
+* @b: parametre
+* Return: the multiple
+*/
+
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * mod - check code
+ * @a: parameter
+ * @b: parameter
+ * Return: modulo
+*/
+int mod(int a, int b)
+{
+    return (a % b);
+}
