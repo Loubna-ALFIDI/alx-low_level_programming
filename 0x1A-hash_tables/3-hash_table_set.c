fb861c38b7ed64b2b20f9a75ef1573e3719f3725
Loubna <loubna.alfidi@gmail.com>
2023-10-06 03:02:06 +0100

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 	{
-		return;
+		return (0);
 	}
 	node->key = strdup(key);
 	node->value = strdup(value);
