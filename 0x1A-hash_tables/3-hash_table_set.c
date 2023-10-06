f8abe7b76974a67342bbb03efb4bcfb6ffc87252
Loubna <loubna.alfidi@gmail.com>
2023-10-06 21:57:46 +0100

    has table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,15 +9,29 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
+	char *current;
 	hash_node_t *node;
 
+	if (ht == NULL || *key == '\0' || key == NULL || value == NULL)
+		return (0);
+	current = strdup(value);
+	if (current == NULL)
+	{
+		return (0);
+	}
 	index = key_index((unsigned char *)key, ht->size);
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 	{
+		free(current);
 		return (0);
 	}
 	node->key = strdup(key);
+	if (node->key == NULL)
+    {
+        free(node);
+        return (0);
+    }
 	node->value = strdup(value);
 	node->next = ht->array[index];
 	ht->array[index] = node;
