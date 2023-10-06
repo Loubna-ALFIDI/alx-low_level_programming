d24283ab0961275b207797ced3c41ed5d827b8f9
Loubna <loubna.alfidi@gmail.com>
2023-10-06 02:55:55 +0100

    hash table set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key of the element
+ * @value: value of the element
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	index = key_index((unsigned char *)key, ht->size);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+	{
+		return;
+	}
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
+}
