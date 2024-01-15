33390439bf0a7d9aa26a2cdd5fe80a904a535d4a
Loubna <loubna.alfidi@gmail.com>
2024-01-15 22:41:07 +0100

    Linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS_h
+#define SEARCH_ALGOS_h
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Functions */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
